e1e557600111c0184da36ac2cc41b662eeab67be
Amit Kumar <amitkumar10311@gmail.com>
2023-03-31 16:30:31 +0200

    Add files via upload

---
+++ Unit 2 | Setting up Environment/unit_02/main.js
@@ -0,0 +1,8 @@
+let vueApp = new Vue({
+    el: "#vueApp",
+    data: {
+        menu_title: 'My menu title',
+        main_title: 'Main title, from Vue!!',
+        phrase: "This is the left side of my web page. It occupies 55% of the total width. Let's practice a bit more using VUE to change the content on the web page."
+    }
+})
